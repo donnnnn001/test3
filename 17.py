8f5f1d1adf438ac149a2516e0a8945588986d19c
Slv <313129046@qq.com>
2020-12-21 11:29:21 +0800

    inin add

---
+++ 17.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from sklearn import metrics
+import matplotlib.pyplot as plt
+%matplotlib inline
